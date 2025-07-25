f81f85c6b169c0a264bf81fe186bf7ea6422f58f
Charles Felipe <charles.fmsilva@gmail.com>
2025-07-25 14:19:57 -0300

    User insert, update and delete

--- src/main/java/com/estudo/bankapp/resources/UserResource.java
+++ src/main/java/com/estudo/bankapp/resources/UserResource.java
@@ -1,12 +1,19 @@
 package com.estudo.bankapp.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudo.bankapp.entites.User;
 import com.estudo.bankapp.services.UserService;
@@ -21,6 +28,32 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		List<User> obj = service.findAll();
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User user){
+		User obj = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<User> deleteById(@PathVariable Long id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}	
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/estudo/bankapp/services/UserService.java
+++ src/main/java/com/estudo/bankapp/services/UserService.java
@@ -1,10 +1,10 @@
 package com.estudo.bankapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.estudo.bankapp.entites.User;
 import com.estudo.bankapp.repositories.UserRepository;
@@ -15,9 +15,33 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	@GetMapping
 	public List<User> findAll(){
 		return repository.findAll();
 	}
 	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		upadateUser(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void upadateUser(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPassword(obj.getPassword());
+		entity.setCpf(obj.getCpf());	
+	}
 }
